7ae08b8cde6c3bd674f3c5221796cb7aea051b0e
Anton Fedurtsya <anton@fedurtsya.com>
2022-09-29 00:39:05 +0300

    Add module meta files and quality tools configurations
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

---
+++ metadata.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '2.1';
+
+/**
+ * Module information
+ */
+$aModule = [
+    'id'          => 'fa_invoice',
+    'title'       => 'Invoice',
+    'description'  => [
+        'en' => 'Invoice module for OXID eShop.',
+    ],
+    'version'     => '0.1.0',
+    'author'       => 'Anton Fedurtsya',
+    'email'        => 'anton@fedurtsya.com',
+    'url'         => '',
+    'extend'      => [
+    ],
+    'settings' => [
+    ],
+];
