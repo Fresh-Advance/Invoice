c91bd94a4b1137001257f71e3b5eea31b4c11784
Anton Fedurtsya <anton@fedurtsya.com>
2022-10-27 23:25:29 +0300

    Add migration with fa_invoices table
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

---
+++ migration/data/Version20221026214950.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Copyright © MB Arbatos Klubas. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace FreshAdvance\Invoice\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20221026214950 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Create Invoices table';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql(
+            "CREATE TABLE `fa_invoices` (
+                    `invoice_id` char(32) NOT NULL,
+                    `invoice_signer` varchar(255) DEFAULT '',
+                    `invoice_date` varchar(100) DEFAULT '',
+                    `invoice_number` varchar(50) DEFAULT '',
+                    `order_id` char(32) NOT NULL,
+                    PRIMARY KEY (`invoice_id`)
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8"
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        // There are no cases invoices table should be removed
+    }
+}
