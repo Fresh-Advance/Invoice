f7ff3c78e4e0be79111f5371b00099f38e78218d
Anton Fedurtsya <anton@fedurtsya.com>
2025-06-08 11:41:51 +0300

    IVM-33 Improve recipe
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

--- recipes/setup-development.sh
+++ recipes/setup-development.sh
@@ -35,13 +35,10 @@ perl -pi\
 mkdir source
 docker compose up --build -d php
 
-git clone https://github.com/Fresh-Advance/Invoice.git ./source -b b-7.1.x
-
 $SCRIPT_PATH/parts/shared/require_shop_edition_packages.sh -e"${edition}" -v"dev-b-7.1.x"
-$SCRIPT_PATH/parts/shared/require_twig_components.sh -e"CE" -b"b-7.1.x"
-$SCRIPT_PATH/parts/shared/require.sh -n"oxid-esales/developer-tools" -v"dev-b-7.1.x"
+$SCRIPT_PATH/parts/shared/require_twig_components.sh -e"${edition}" -b"b-7.1.x"
 $SCRIPT_PATH/parts/shared/require.sh -n"oxid-esales/twig-theme" -v"dev-b-7.1.x"
-$SCRIPT_PATH/parts/shared/require_demodata_package.sh -e"CE" -b"b-7.1.x"
+$SCRIPT_PATH/parts/shared/require_demodata_package.sh -e"${edition}" -b"b-7.1.x"
 
 docker compose exec php composer update --no-interaction
 
