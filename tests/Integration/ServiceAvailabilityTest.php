c9c7195f1fdd624446224047dada50710325cac3
Anton Fedurtsya <anton@fedurtsya.com>
2025-06-21 16:41:26 +0300

    Add more classes to availability test
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

--- tests/Integration/ServiceAvailabilityTest.php
+++ tests/Integration/ServiceAvailabilityTest.php
@@ -21,6 +21,7 @@ class ServiceAvailabilityTest extends IntegrationTestCase
     {
         $container = (new TestContainerFactory())->create();
         $container->compile(true);
+        $container->get('oxid_esales.module.install.service.launched_shop_project_configuration_generator')->generate();
 
         self::$cachedContainer = $container;
     }
@@ -48,8 +49,34 @@ class ServiceAvailabilityTest extends IntegrationTestCase
             [\FreshAdvance\Invoice\Order\Settings\OrderSettingsInterface::class],
 
             // Pdf
+            // todo: find out how to check the module settings using instance
 //            [\FreshAdvance\Invoice\Pdf\InvoiceGeneratorInterface::class],
 //                [\FreshAdvance\Invoice\Order\Decoration\InvoiceGeneratorDecorator::class],
+            [\FreshAdvance\Invoice\Pdf\Service\DocumentRendererInterface::class],
+            [\FreshAdvance\Invoice\Pdf\Service\FormatCalculatorInterface::class],
+            [\FreshAdvance\Invoice\Pdf\Service\FilenameCalculatorInterface::class],
+                [\FreshAdvance\Invoice\Pdf\Service\FilenameLengthFilter::class],
+                [\FreshAdvance\Invoice\Pdf\Service\FilenameCharactersFilter::class],
+            [\FreshAdvance\Invoice\Pdf\Service\TemplateParametersServiceInterface::class],
+            [\FreshAdvance\Invoice\Pdf\Settings\DocumentLayoutSettingsInterface::class],
+
+            // todo: move shared repository items to where they belong.
+            [\FreshAdvance\Invoice\Repository\InvoiceConfigurationRepositoryInterface::class],
+                [\FreshAdvance\Invoice\Repository\InvoiceConfigurationRepositoryDecoration::class],
+            [\FreshAdvance\Invoice\Repository\ShopRepositoryInterface::class],
+
+            // todo: move shared service items to where they belong.
+            [\FreshAdvance\Invoice\Service\Invoice::class],
+            [\FreshAdvance\Invoice\Service\InvoiceServiceInterface::class],
+
+            // Settings
+            [\FreshAdvance\Invoice\Settings\ContextInterface::class],
+            [\FreshAdvance\Invoice\Settings\ModuleSettingsInterface::class],
+            [\FreshAdvance\Invoice\Settings\ConfigInterface::class],
+
+            // Transput
+            [\FreshAdvance\Invoice\Transput\RequestInterface::class],
+            [\FreshAdvance\Invoice\Transput\UtilsInterface::class],
         ];
     }
 }
